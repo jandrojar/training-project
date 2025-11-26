636f2818087c47bc170fec3e08a1c06bff616f81
Alejandro Jardón <ajardonbueno@gmail.com>
2025-11-26 16:35:49 +0100

    feat: add basic Koa server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Backend setup");
+import Koa from "koa";
+import Router from "@koa/router";
+import bodyParser from "koa-bodyparser";
+
+const app = new Koa();
+const router = new Router();
+
+router.get("/", (ctx) => {
+  ctx.body = "API is running";
+});
+
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
