5cd87f4e284190e6a3c37564b245231049aa4834
Alejandro Jardón <ajardonbueno@gmail.com>
2025-11-27 13:09:39 +0100

    feat: add login/logout controllers and auth routes

---
+++ backend/src/routes/authRoutes.ts
@@ -0,0 +1,11 @@
+import Router from "@koa/router";
+import { loginHandler, logoutHandler } from "../controllers/authController";
+
+const authRouter = new Router({
+    prefix: "/auth",
+})
+
+authRouter.post("/login", loginHandler);
+authRouter.post("/logout", logoutHandler);
+
+export default authRouter;
